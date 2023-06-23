02c0cec7dc485ef0a08ad1b1d6537d71dc0a5e24
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-06-22 23:31:52 -0600

    animaciones listas, falta seccion de proyectos y diseño

--- portafolio-3/js/script.js
+++ portafolio-3/js/script.js
@@ -38,4 +38,10 @@ window.onscroll = () => {
 
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
+
+  let footer = document.querySelector("footer");
+  footer.classList.toggle(
+    "show-animate",
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
 };
