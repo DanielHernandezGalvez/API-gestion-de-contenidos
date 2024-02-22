79bde1c5ba2aa289caa3fca8a1b60fc2f132eee7
unknown <hernandezgalvezalejandro@gmail.com>
2024-02-22 15:41:16 -0600

    hero movill

--- views/index_view.php
+++ views/index_view.php
@@ -6,7 +6,19 @@
     <title>Document</title>
     <link rel="stylesheet" href="/styles.css">
 </head>
-<body class="container">
+<body class="">
+    <header>
+
+    </header>
+
+    <section class="hero">
+        <div>
+        <p>Desarrollo y Diseño web Freelance</p>
+        <h1>Creador Web</h1>
+        <button>COTIZAR</button>
+        </div>
+    </section>
+
     <?php require "form_view.php"; ?>
 </body>
 </html>
