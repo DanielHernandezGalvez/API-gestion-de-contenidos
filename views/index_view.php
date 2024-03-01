06763bb38f714948527b4e6835348ea6d1873c6b
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-01 17:08:10 -0600

    hero

--- views/index_view.php
+++ views/index_view.php
@@ -1,24 +1,44 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="/styles.css">
+    <link rel="shortcut icon" href="../FAVICON.png" type="image/x-icon">
 </head>
+
 <body class="">
     <header>
+        <nav class="navigation">
+            <img src="../img/LOGO.svg" alt="logo del creador web">
+            <div class="hamburger-slim">
 
+            </div>
+        </nav>
     </header>
 
     <section class="hero">
         <div>
-        <p>Desarrollo y Diseño web Freelance</p>
-        <h1>Creador Web</h1>
-        <button>COTIZAR</button>
+            <p>Desarrollo y Diseño web Freelance</p>
+            <h1>Creador Web</h1>
+            <button>COTIZAR</button>
         </div>
+
+     
+            <div class="social-media">
+
+                <img src="../icons/facebook.png" alt="facebook">
+                <img src="../icons/instagram.png" alt="instagram">
+                <img src="../icons/linkedin.png" alt="linkedin">
+
+            </div>
+ 
+
     </section>
 
     <?php require "form_view.php"; ?>
 </body>
+
 </html>
