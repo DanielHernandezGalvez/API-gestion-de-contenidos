50499c32970c115c4f6df3fb8e97ce1904f1aa3c
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-02 12:36:06 -0600

    services movile ready

--- views/services_view.php
+++ views/services_view.php
@@ -1,12 +1,46 @@
 <section class="services">
-    <div>
+    <div style=" box-shadow: 2px 2px 8px #22272b; z-index: 1;">
         <h3 class="title">Servicios</h3>
     </div>
 
     <div class="services-grid">
-        <div><img src="https://www.freepik.com/free-photo/homepage-seen-computer-screen_26538661.htm#fromView=search&page=2&position=5&uuid=8f3e5faa-3a7a-46f3-a4c3-0adbd4921509" alt=""></div>
-        <div><img src="" alt="">
-            <span></span>
+        <div class="services-row services-bg-dark">
+
+            <div>
+                <img src="https://img.freepik.com/free-psd/laptop-mock-up-design_1315-12.jpg?t=st=1709403342~exp=1709406942~hmac=9cdcc95f3ce93f3b00221a13a2e95f7763ea674705abbd0d548c2e04fb0b9aeb&w=740" alt="Página web">
+            </div>
+
+            <div>
+                <img src="../icons/world.png" alt="">
+                <b>Páginas web</b>
+            </div>
+
         </div>
+        <div class="services-row-reverse">
+
+
+            <div>
+                <img src="../icons/design.png" alt="Diseño ux ui">
+                <b>Diseño UX UI</b>
+            </div>
+
+            <div>
+                <img src="https://img.freepik.com/free-photo/ui-ux-representations-with-laptop_23-2150201871.jpg?t=st=1709403034~exp=1709406634~hmac=a44ef68aa8ed950217f531667014f55882928cceef8ea8196d35e59b1c60ea2e&w=740" alt="Diseño ux ui">
+            </div>
+
+        </div>
+        <div class="services-row services-bg-purple">
+
+            <div>
+                <img src="https://img.freepik.com/free-photo/data-server-racks-hub-room-with-big-data-computer-center-blue-interior-hosting-storage-hardware_90220-1033.jpg?t=st=1709403123~exp=1709406723~hmac=eb6fd397ac1ebea47f3e4545d98aec52efe4ea62a6580c6a4bebb0c3fb1d3073&w=740" alt="Hospedaje web">
+            </div>
+
+            <div>
+                <img src="../icons/cloud.png" alt="Hosting">
+                <b>Hosting y registro de dominios</b>
+            </div>
+
+        </div>
+
     </div>
 </section>
