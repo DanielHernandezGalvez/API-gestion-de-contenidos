12b7562e996c4a3f114f5da174d391209eef476c
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-04 10:30:46 -0600

    seo

--- views/index_view.php
+++ views/index_view.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Página web pagina web barata pagina web precio">
+    <meta name="description" content="Desarrollo y diseño de paginas web guadalajara gdl">
     <title>El Creador Web</title>
     <link rel="stylesheet" href="./assets/styles.css">
     <link rel="shortcut icon" href="../assets/FAVICON.png" type="image/x-icon">
