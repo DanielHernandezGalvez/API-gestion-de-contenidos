8b57f7c216ac7893555bb9fcc71e9329172bc53a
unknown <hernandezgalvezalejandro@gmail.com>
2024-03-12 15:55:32 -0600

    gradient

--- views/services_view.php
+++ views/services_view.php
@@ -6,7 +6,8 @@
     <div class="services-grid">
         <div class="services-row services-bg-dark">
             <div class="box">
-                <img src="https://img.freepik.com/free-psd/laptop-mock-up-design_1315-12.jpg?t=st=1709403342~exp=1709406942~hmac=9cdcc95f3ce93f3b00221a13a2e95f7763ea674705abbd0d548c2e04fb0b9aeb&w=740" alt="Página web">
+                <!-- <img src="https://img.freepik.com/free-psd/laptop-mock-up-design_1315-12.jpg?t=st=1709403342~exp=1709406942~hmac=9cdcc95f3ce93f3b00221a13a2e95f7763ea674705abbd0d548c2e04fb0b9aeb&w=1080" alt="Página web"> -->
+                <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Página web">
                 <span class="over-text">Podemos hacer tu página web a la mdedida y enfocada en tus necesidades digitales</span>
             </div>
             <div>
@@ -22,14 +23,14 @@
             </div>
             <div class="box">
                 <img src="https://img.freepik.com/free-photo/ui-ux-representations-with-laptop_23-2150201871.jpg?t=st=1709403034~exp=1709406634~hmac=a44ef68aa8ed950217f531667014f55882928cceef8ea8196d35e59b1c60ea2e&w=740" alt="Diseño ux ui">
-                <span class="over-text">Podemos hacer tu página web a la mdedida y enfocada en tus necesidades digitales</span>
+                <span class="over-text">Diseñamos interfaces efectivas y atractivas para tu sitio web</span>
             </div>
         </div>
 
         <div class="services-row services-bg-purple">
             <div class="box">
                 <img src="../assets/img/server.jpg" alt="Hospedaje web">
-                <span class="over-text">Podemos hacer tu página web a la mdedida y enfocada en tus necesidades digitales</span>
+                <span class="over-text">No necesitas comprar hospedaje ni dominio, nosotros lo hacemos por tí</span>
             </div>
             <div>
                 <img width="40" src="../assets/icons/cloud.svg" alt="Hosting">
