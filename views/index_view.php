4ef8d1d42e65be2a35368e000ba1b7be1b28805b
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-03-20 23:16:25 -0600

    Update index_view.php

--- views/index_view.php
+++ views/index_view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="Página web pagina web barata pagina web precio">
+    <meta name="keywords" content="Página web pagina web barata cuanto cuesta pagina web ">
     <meta name="description" content="Desarrollo y diseño de paginas web guadalajara gdl">
     <title>El Creador Web</title>
     <link rel="stylesheet" href="./assets/styles.css">
