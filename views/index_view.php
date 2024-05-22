154453ddd887b00a9b7f678dadbc7e2cdfa37ea6
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-05-22 14:00:52 -0600

    Update index_view.php

--- views/index_view.php
+++ views/index_view.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="Página web pagina web barata cuanto cuesta pagina web ">
-    <meta name="description" content="Desarrollo y diseño de paginas web guadalajara gdl">
+    <meta name="keywords" content="Página web, diseño de página web, pagina web precio">
+    <meta name="description" content="Desarrollo y diseño de paginas web en Guadalajara">
+    <meta name="robots" content="index, follow">
     <title>Diseño de páginas web. Te hacemos tu página web a la medida. - El Creador Web</title>
     <link rel="stylesheet" href="./assets/styles.css">
     <link rel="shortcut icon" href="../assets/favicon.svg" type="image/x-icon">
