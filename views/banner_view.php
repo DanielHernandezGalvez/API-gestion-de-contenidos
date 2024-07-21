71cdbe16bfdfef683383a636e6e4ddd3f3a79286
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-07-21 17:59:39 -0600

    Update banner_view.php

--- views/banner_view.php
+++ views/banner_view.php
@@ -1,5 +1,4 @@
 <section class="banner">
     <div>Texto de la parte de arriba, seecion de redes sociales, en celular hay que desaparecer</div>
-
     <div>en esta parte ban a ir los clientes, el contador de visitas y l contacto, hay que ver que se requiere</div>
 </section>
