f8e597549183e3a2783ffb13e6bad073d8dabc38
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-08-22 13:01:40 -0600

    Update about_view.php

--- views/about_view.php
+++ views/about_view.php
@@ -5,7 +5,8 @@
         <p>
             En Creador Web, somos apasionados por crear sitios y aplicaciones web que te ayuden a alcanzar tus objetivos. <br>
             Estamos aquí para ayudarte a crear una presencia online que sea atractiva, fácil de usar y que te ayude a generar resultados.
-            Si eres una agencia de marketing o publicidad y necesitas quieres dar el servicio de desarrollo web contáctanos, somos tu mejor opción.
+            Si eres una agencia de marketing o publicidad y necesitas quieres dar el servicio de desarrollo web contáctanos, 
+            somos tu mejor opción.
         </p>
         <img src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_640.jpg" alt="Desarrollador web">
     </div>
